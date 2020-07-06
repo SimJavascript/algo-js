ac1e1e87c14a91716cc4692108aaa81f689467b4
simonduquaine <duquaine.simon@gmail.com>
2020-07-06 16:58:02 +0200

    js

---
+++ 01-intro/exercice1-1.js
@@ -0,0 +1,4 @@
+const readlineSync = require("readline-sync");
+
+let myVar = "Simon";
+console.log(myVar);
---
+++ 01-intro/exercice1-2.js
@@ -0,0 +1,7 @@
+const readlineSync = require("readline-sync");
+
+let name = "Simon";
+let firstName= "Duquaine";
+let city = "Charleroi";
+
+console.log("Your name is " + name + " " + firstName + " and you live in " + city + ".");
